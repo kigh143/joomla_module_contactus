eb78ce90723eadc357dbdcccfddcb8078127afbc
Katende Ivan <kigh143@gmail.com>
2016-03-15 23:57:26 +0300

    formatting code

--- install.sql
+++ install.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `#__mod_xaexiamail` (
   `id` int(10) NOT NULL  AUTO_INCREMENT,
-    `name` varchar(150) NOT NULL,
-      `email` varchar(150) NOT NULL,
-        `message` text NOT NULL,
-	  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	     PRIMARY KEY (`id`)
-	     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+   `name` varchar(150) NOT NULL,
+   `email` varchar(150) NOT NULL,
+   `message` text NOT NULL,
+   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
